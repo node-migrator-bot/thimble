32413d6772aa33297a26ce7f2454b921893034ff
Matt Mueller <mattmuelle@gmail.com>
2012-01-18 01:09:32 -0800

    added bundle and package to both

--- examples/documentation/app.js
+++ examples/documentation/app.js
@@ -18,8 +18,8 @@ thimble(options);
 
 thimble.configure(function(use) {
   use(thimble.flatten());
-  // use(thimble.bundle());
-  // use(thimble.package());
+  use(thimble.bundle());
+  use(thimble.package());
 });
 
 // Start thimble
