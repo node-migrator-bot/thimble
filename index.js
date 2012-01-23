7388b12c1e7fc2cb23385359187683f5dd380fd3
Matt Mueller <mattmuelle@gmail.com>
2012-01-22 23:05:09 -0800

    require path now considers cwd node_modules

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+// Add current directory node_modules to require paths
+require.paths.unshift(join(process.cwd(), 'node_modules'));
+
+// Load thimble
 module.exports = require(__dirname + '/lib/thimble.js');
