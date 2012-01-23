0996986985c050b25012e409f459d16d27707dcf
Matt Mueller <mattmuelle@gmail.com>
2012-01-22 23:07:12 -0800

    moved require.paths

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-// Add current directory node_modules to require paths
-require.paths.unshift(join(process.cwd(), 'node_modules'));
-
 // Load thimble
 module.exports = require(__dirname + '/lib/thimble.js');
